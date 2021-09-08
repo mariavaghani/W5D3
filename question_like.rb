14355c972fa94bbc5dacea4c4ffdbd9796a0cdc7
mariavaghani <maria.vaghani@gmail.com>
2021-09-08 16:31:44 -0400

    Add some QuestionLike methods

--- question_like.rb
+++ question_like.rb
@@ -16,9 +16,57 @@ class QuestionLike
         options.map { |option| QuestionLike.new(option) }
     end
 
+    def self.likers_for_question_id(question_id)
+        options = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+        SELECT
+            user_id AS id, fname, lname
+        FROM
+            users
+        JOIN
+            question_likes ON users.id = question_likes.user_id
+        WHERE
+            question_id = ?
+        SQL
+        options.map { |option| User.new(option) }
+    end
+
+    def self.num_likes_for_question_id(question_id)
+        result = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+        SELECT
+            COUNT(*)
+        FROM
+            question_likes
+        WHERE
+            question_id = ?
+        SQL
+        result.first.values.first
+    end
+
+    def self.liked_questions_for_user_id(user_id)
+        options = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE
+            id in (
+                SELECT
+                    question_id
+                FROM
+                    question_likes
+                WHERE
+                    user_id = ?
+            )
+        
+        SQL
+        options.map { |option| Question.new(option) }
+    end
+
     def initialize(options)
         @id = options['id']
         @user_id = options['user_id']
         @question_id = options['question_id']
     end
+
+
 end
